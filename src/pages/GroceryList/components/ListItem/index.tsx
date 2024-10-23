47d40023cf587e2fa451b1b8fe198ca70a138ac7
Nikita Kurochka <freenikita95@gmail.com>
2024-10-22 21:50:57 -0700

    refactoring to increase performens

---
+++ src/pages/GroceryList/components/ListItem/index.tsx
@@ -0,0 +1,88 @@
+import { memo, MouseEvent } from "react";
+import { ListItemIcon } from "@mui/material";
+import Checkbox from "@mui/material/Checkbox";
+import MUIListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import groceryAPI from "api/grocery";
+import { TGroceryList, TGroceryListItem } from "types/grocery";
+
+import SettingAction from "components/SettingAction";
+import queryKey from "constants/query";
+
+interface IListItem {
+  id: string;
+  title: string;
+  amount: number;
+  completed: boolean;
+  handleOpenActions: (
+    itemId: string
+  ) => (event: MouseEvent<HTMLButtonElement>) => void;
+}
+
+function ListItem(props: IListItem) {
+  const { id, completed, amount, title, handleOpenActions } = props;
+  const queryClient = useQueryClient();
+  const key = `${id}-checkbox-${completed}`;
+  const textDecoration = completed ? "line-through" : "none";
+
+  const data = queryClient.getQueryData([
+    `${queryKey.lists}/${id}`,
+  ]) as TGroceryList;
+  const { list } = data ?? { list: [] };
+
+  const updateItemsMutation = useMutation({
+    mutationKey: [`${queryKey.lists}/${id}`],
+    mutationFn: (newList: TGroceryListItem[]) =>
+      groceryAPI.updateListItems(id, newList),
+    onSuccess: (newValue: TGroceryList) => {
+      queryClient.setQueryData(
+        [`${queryKey.lists}/${id}`],
+        (old: TGroceryList[]) => ({
+          ...old,
+          list: newValue.list,
+        })
+      );
+    },
+  });
+
+  const checkedItem = (itemId: string) => () => {
+    const newLists: TGroceryListItem[] = list.map((task: TGroceryListItem) => {
+      if (task.id === itemId) {
+        return {
+          ...task,
+          completed: !task.completed,
+        };
+      }
+      return task;
+    });
+    updateItemsMutation.mutate(newLists);
+  };
+
+  return (
+    <MUIListItem
+      secondaryAction={<SettingAction onClick={handleOpenActions(id)} />}
+    >
+      <ListItemIcon>
+        <Checkbox
+          edge="start"
+          checked={completed}
+          tabIndex={-1}
+          disableRipple
+          inputProps={{ "aria-labelledby": key }}
+          onClick={checkedItem(id)}
+        />
+      </ListItemIcon>
+      <ListItemText
+        id={key}
+        primary={<div style={{ textDecoration }}>{title}</div>}
+        secondary={amount !== 0 ? amount : undefined}
+      />
+    </MUIListItem>
+  );
+}
+
+export default memo(
+  ListItem,
+  (prevProps, nextProps) => prevProps.id === nextProps.id
+);
--- src/pages/GroceryList/components/ListItems/index.tsx
+++ src/pages/GroceryList/components/ListItems/index.tsx
@@ -1,17 +1,13 @@
 import { MouseEvent } from "react";
 import { useParams } from "next/navigation";
-import { ListItemIcon } from "@mui/material";
-import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid2";
 import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import groceryAPI from "api/grocery";
+import { useQueryClient } from "@tanstack/react-query";
 import { TGroceryList, TGroceryListItem } from "types/grocery";
 
-import SettingAction from "components/SettingAction";
 import queryKey from "constants/query";
+
+import ListItem from "../ListItem";
 
 interface IListItems {
   handleOpenActions: (
@@ -21,73 +17,29 @@ interface IListItems {
 
 function ListItems(props: IListItems) {
   const { handleOpenActions } = props;
+  const queryClient = useQueryClient();
+
   const params = useParams<{ id: string }>();
   const { id } = params || { id: "" };
-  const queryClient = useQueryClient();
+
   const data = queryClient.getQueryData([
     `${queryKey.lists}/${id}`,
   ]) as TGroceryList;
   const { list } = data;
 
-  const updateItemsMutation = useMutation({
-    mutationKey: [`lists/${id}`],
-    mutationFn: (newList: TGroceryListItem[]) =>
-      groceryAPI.updateListItems(id, newList),
-    onSuccess: (newValue: TGroceryList) => {
-      queryClient.setQueryData(
-        [`${queryKey.lists}/${id}`],
-        (old: TGroceryList[]) => ({
-          ...old,
-          list: newValue.list,
-        })
-      );
-    },
-  });
-
-  const checkedItem = (itemId: string) => () => {
-    const newLists: TGroceryListItem[] = list.map((task: TGroceryListItem) => {
-      if (task.id === itemId) {
-        return {
-          ...task,
-          completed: !task.completed,
-        };
-      }
-      return task;
-    });
-    updateItemsMutation.mutate(newLists);
-  };
-
   return (
     <Grid size={12}>
       <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-        {list.map((task: TGroceryListItem) => {
-          const key = `checkbox-${task.completed}-${task.id}`;
-          const textDecoration = task.completed ? "line-through" : "none";
-          return (
-            <ListItem
-              key={key}
-              secondaryAction={
-                <SettingAction onClick={handleOpenActions(task.id)} />
-              }
-            >
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={task.completed}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": key }}
-                  onClick={checkedItem(task.id)}
-                />
-              </ListItemIcon>
-              <ListItemText
-                id={key}
-                primary={<div style={{ textDecoration }}>{task.title}</div>}
-                secondary={task.amount !== 0 ? task.amount : undefined}
-              />
-            </ListItem>
-          );
-        })}
+        {list.map((task: TGroceryListItem) => (
+          <ListItem
+            key={task.id}
+            id={task.id}
+            completed={task.completed}
+            amount={task.amount}
+            title={task.title}
+            handleOpenActions={handleOpenActions}
+          />
+        ))}
       </List>
     </Grid>
   );
