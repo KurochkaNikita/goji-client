e46c89a775b30e17a62011122b643cb45db616df
Nikita Kurochka <freenikita95@gmail.com>
2024-10-22 23:54:55 -0700

    refactoring

--- src/validation/grocery.ts
+++ src/validation/grocery.ts
@@ -1,13 +1,16 @@
 import * as yup from "yup";
 
+const minTaskTitle = 5;
+const maxTaskTitle = 25;
+
 const groceryListItemForm = yup
   .object({
     title: yup
       .string()
       .test(
         "len",
-        "Must be from 5 to 15",
-        (val = "") => val.length > 5 && val.length < 15
+        `Must be from ${minTaskTitle} to ${maxTaskTitle}`,
+        (val = "") => val.length > minTaskTitle && val.length < maxTaskTitle
       )
       .required("Enter title from"),
     amount: yup.number().min(0, "Enter 0 or any positive number."),
